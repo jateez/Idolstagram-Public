590a29008976c0ed967f528768be5aca8831f610
Arya Wibowo Jati <aryajati29@gmail.com>
2024-08-20 10:24:23 +0700

    feat: add helpers jwt bcrypt and emailValidator

--- server/helpers/bcrypt.js
+++ server/helpers/bcrypt.js
@@ -1,7 +1,7 @@
 const bcrypt = require("bcryptjs");
 
 const hashPassword = (password) => {
-  const salt = bcrypt.saltSync()
+  const salt = bcrypt.genSaltSync()
   return bcrypt.hashSync(password, salt)
 }
 
---
+++ server/helpers/emailValidator.js
@@ -0,0 +1,5 @@
+const emailValidator = (email) => {
+  return /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(email)
+}
+
+module.exports = emailValidator
