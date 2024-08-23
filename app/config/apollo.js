51b585ae8ef788cfb804137e3be7262cbd685d20
Arya Wibowo Jati <aryajati29@gmail.com>
2024-08-23 11:13:08 +0700

    feat: add apollo config

---
+++ app/config/apollo.js
@@ -0,0 +1,25 @@
+const { ApolloClient, InMemoryCache, createHttpLink } = require("@apollo/client");
+import { setContext } from "@apollo/client/link/context"
+import * as secureStore from "expo-secure-store"
+
+const httpLink = createHttpLink({
+  uri: "https://37f5-103-165-209-195.ngrok-free.app/"
+})
+
+const authLink = setContext(async (_, { headers }) => {
+  const token = await secureStore.getItemAsync("access_token")
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ""
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
+
+export default client
